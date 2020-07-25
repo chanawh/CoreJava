88c824a71c17c32974f9f6f582d07acfc7aba145
chanawh <thechanwh@gmail.com>
2020-07-25 16:48:07 -0700

    added raiseSalary method

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -9,6 +9,11 @@ public class EmployeeTest {
         staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
         staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
 
+        for (Employee e: staff)
+        {
+            e.raiseSalary(5);
+        }
+
         for (Employee e : staff) {
             System.out.println("name=" + e.getName() + ", " + "salary= " + e.getSalary() + ", " + "hireDay=" + e.getHireDay());
         }
@@ -44,5 +49,11 @@ class Employee
     public LocalDate getHireDay()
     {
         return hireDay;
+    }
+
+    public void raiseSalary (double byPercent)
+    {
+        double raise = salary * byPercent / 100;
+        salary += raise;
     }
 }
